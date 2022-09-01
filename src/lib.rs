a59a7ebb601fd99473cbadd916442ca12577c3ee
y1j2x34 <y1j2x34@qq.com>
2022-09-01 23:20:10 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,23 @@
 #![allow(non_snake_case)]
 
 extern crate wasm_bindgen;
+extern crate serde_json;
 
-use js_sys::{Object, Reflect, Math};
+use std::{collections::HashMap};
+
+use js_sys::{ Math };
 use wasm_bindgen::{prelude::*, __rt::WasmRefCell};
 
+type Rule = HashMap<String, HashMap<String, f64>>;
 
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
+}
 #[derive(Clone)]
 pub struct Atom {
     x: f64,
@@ -19,22 +31,36 @@ pub struct Atom {
 pub struct ParticleWord {
     pub atoms: Vec<Atom>,
     pub width: i32,
-    pub height: i32
+    pub height: i32,
+    pub rule: Rule
 }
 
-type Rule = Object;
 
 impl ParticleWord {
 
-    pub fn new(width: i32, height: i32) -> Self {
+    pub fn new(width: i32, height: i32, ruleJson: String) -> Self {
+        let ruleResult: Result<Rule, _> = serde_json::from_str(ruleJson.as_str());
+        // let rule: Rule = ruleObject.into_serde().unwrap();
+        // let ruleResult: Result<Rule, _> = serde_wasm_bindgen::from_value(ruleObject);
+
+        if let Err(_) = ruleResult {
+            error(format!("{:?}", &ruleResult).as_str());
+            panic!("error");
+        }
+        let rule = ruleResult.unwrap();
+        // log(format!("rule: {:?}", &rule));
+
         ParticleWord {
             atoms: Vec::new(),
             width,
-            height
+            height,
+            rule
         }
     }
 
-    fn apply_rules(& mut self, rule: &Rule) {
+    fn apply_rules(& mut self) {
+        let rule = &self.rule;
+
         let size = self.atoms.len();
 
         let mut i = 0;
@@ -68,23 +94,35 @@ impl ParticleWord {
 
                 
 
-                let ri = Reflect::get(rule, &JsValue::from_str(atom_i.color.as_str())) ;
-                if let Err(_) = ri {
+                // let ri = Reflect::get(rule, &JsValue::from_str(atom_i.color.as_str())) ;
+                // if let Err(_) = ri {
+                //     continue;
+                // }
+                let ri = rule.get(atom_i.color.as_str());
+
+                if let None = ri {
                     continue;
                 }
+                let rj_opt = ri.unwrap().get(atom_j.color.as_str());
                 
-
-                let rj_opt = Reflect::get(&ri.unwrap(), &JsValue::from_str(atom_j.color.as_str()));
-
-                if let Err(_) = rj_opt {
+                if let None = rj_opt {
                     continue;
                 }
-                let og = rj_opt.unwrap().as_f64();
 
-                if let None = og {
-                    continue;
-                }
-                let g = og.unwrap();
+                // let rj_opt = Reflect::get(&ri.unwrap(), &JsValue::from_str(atom_j.color.as_str()));
+
+                // if let Err(_) = rj_opt {
+                //     continue;
+                // }
+
+                // let og = rj_opt.unwrap().as_f64();
+
+                // if let None = og {
+                //     continue;
+                // }
+                // let g = og.unwrap();
+
+                let g = rj_opt.unwrap();
 
                 let dx = atom_i.x - atom_j.x;
                 let dy = atom_i.y - atom_j.y;
@@ -144,19 +182,20 @@ impl ParticleWord {
 }
 
 
-#[export_name = "ParticleWord_new"]
-pub extern "C" fn __wasm_bindgen_generated_ParticleWord_new(width: i32, height: i32) -> u32 {
-    let particle_word = ParticleWord::new(width, height);
+#[wasm_bindgen]
+pub fn ParticleWord_new(width: i32, height: i32, ruleJson: String) -> u32 {
+    log(format!("rule json: {:?}, width: {}, height: {} ", ruleJson, width, height).as_str());
+    let particle_word = ParticleWord::new(width, height, ruleJson);
     Box::into_raw(Box::new(WasmRefCell::new(particle_word))) as u32
 }
 
 #[export_name = "ParticleWord_apply_rules"]
-pub extern "C" fn __wasm_bindgen_generated_ParticleWord_apply_rules(ptr: u32, rule: &Rule) {
+pub extern "C" fn __wasm_bindgen_generated_ParticleWord_apply_rules(ptr: u32) {
     let instance = ptr as *mut WasmRefCell<ParticleWord>;
     wasm_bindgen::__rt::assert_not_null(instance);
     let instance = unsafe { &* instance };
 
-    instance.borrow_mut().apply_rules(rule);
+    instance.borrow_mut().apply_rules();
 }
 
 #[no_mangle]
